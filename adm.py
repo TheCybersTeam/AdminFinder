534b3db8a493d7fe10c4f5f0c3be4a514d9cb952
The Cybers Team <roohbots@gmail.com>
2016-05-11 23:24:27 -0300

    Beta 0.2

--- adm.py
+++ adm.py
@@ -1,14 +1,18 @@
 # Facebook: https://www.facebook.com/TheCybersTeam
 # Group: 	https://www.facebook.com/groups/TheCybersTeam
 # Channel:  https://www.youtube.com/channel/UCKFMv1cifW55lKKps2thhbw
-# Version: Beta 0.1
+# Version: Beta 0.2
 
 import urllib as cybers
 
-url = "http://www.target.com.br/admin"
-res = cybers.urlopen(url)
+url = "http://target.com.br/"
+directories = ["adm", "painel", "administrador", "login", "admin"]
 
-if res.getcode() == 200:
-	print "Found!"
-else:
-	print "Not found!"
+for i in directories:
+	target = url + i
+	res = cybers.urlopen(target)
+
+	if res.getcode() == 200:
+		print "[+] " + target 
+	else:
+		print "[-] " + target
