801aa9b7ce0ae45b90a66166205ec95993faee74
g3ol4d0 <geolado@users.noreply.github.com>
2016-05-12 02:23:28 -0300

    Functional programming
    http://www.devfuria.com.br/python/entenda-__name__-__main__/

--- adm.py
+++ adm.py
@@ -6,30 +6,36 @@
 import urllib as cybers
 import sys
 
-print "_____ _              ___      _                     _____                      "
-print "/__   \ |__   ___    / __\   _| |__   ___ _ __ ___  /__   \___  __ _ _ __ ___  "
-print "  / /\/ '_ \ / _ \  / / | | | | '_ \ / _ \ '__/ __|   / /\/ _ \/ _` | '_ ` _ \ "
-print " / /  | | | |  __/ / /__| |_| | |_) |  __/ |  \__ \  / / |  __/ (_| | | | | | |"
-print " \/   |_| |_|\___| \____/\__, |_.__/ \___|_|  |___/  \/   \___|\__,_|_| |_| |_|"
-print "                         |___/                                                 Beta 0.3"
-
 directories = ["adm", "painel", "administrador", "login", "admin"]
 
-url = sys.argv[1]
-
-try:
-	cybers.urlopen(url)
-	print "\nConection OK!"
-	print "Initializing the search...\n"
-except:
-	print "Fail, try it: \npython admin.py http://www.yourtarget.com.br"
-	exit()
+def admin_finder(url) :
+	try:
+		cybers.urlopen(url)
+		print "\nConection OK!"
+		print "Initializing the search...\n"
+	except:
+		print "Fail, try it: \npython admin.py http://www.yourtarget.com.br"
+		exit()
+			
+	for i in directories:
+		target = url + "/" + i
+		res = cybers.urlopen(target)
+	
+		if res.getcode() == 200:
+			print "[+] " + target 
+		else:
+			print "[-] " + target
 		
-for i in directories:
-	target = url + "/" + i
-	res = cybers.urlopen(target)
+def main() :
+	print "_____ _              ___      _                     _____                      "
+	print "/__   \ |__   ___    / __\   _| |__   ___ _ __ ___  /__   \___  __ _ _ __ ___  "
+	print "  / /\/ '_ \ / _ \  / / | | | | '_ \ / _ \ '__/ __|   / /\/ _ \/ _` | '_ ` _ \ "
+	print " / /  | | | |  __/ / /__| |_| | |_) |  __/ |  \__ \  / / |  __/ (_| | | | | | |"
+	print " \/   |_| |_|\___| \____/\__, |_.__/ \___|_|  |___/  \/   \___|\__,_|_| |_| |_|"
+	print "                         |___/                                                 Beta 0.3"
 
-	if res.getcode() == 200:
-		print "[+] " + target 
-	else:
-		print "[-] " + target
+	user_url = sys.argv[1]
+	admin_finder(user_url)
+
+if __name__ == "__main__":
+	main()
